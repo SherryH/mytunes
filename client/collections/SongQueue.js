1cca3110298992cc2115397a818860f92dfd714c
SherryH <enjoy.wind7rain@gmail.com>
2017-01-05 17:54:17 -0800

    Passed all tests but html still not rendering properly

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,31 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    });
+    this.on('ended', function() {
+      this.shift();
+      if (this.models.length > 0) {
+        this.playFirst();
+      }
+    });
+    this.on('dequeue', function(e) {
+      this.remove(this.get(e.cid));
+    });
+    this.on('change', function() {
+      console.log('enqueued');
+    });
+    this.on('enqueue', function(e) {
+      console.log('hi');
+      console.log(this);
+      this.push(this.get(e.cid));
+    });
+  },
+  playFirst: function() {
+    this.models[0].play();
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,21 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+
+    this.collection.on('add', this.render, this);
+
+    this.collection.on('remove', this.render, this);
+    this.render();
   },
 
   render: function() {
+    this.collection.forEach(function(song) {
+      var songQueueEntryView = new SongQueueEntryView({model:song});
+      this.$el.append(songQueueEntryView.render());
+    }, this);
+    //console.log(this);
+    //call SongQueueEntryView render
+    // loop through the queues
     return this.$el;
   }
 
